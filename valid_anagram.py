9f452ff8ce75e8c336b5610aec8f0dd06a4e4c27
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-08-31 21:00:39 +0200

    added solution to valid anagram problem

---
+++ valid_anagram.py
@@ -0,0 +1,34 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+'''
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        
+        if len(s) != len(t):
+            return False
+        else:
+            sorted_s = ''.join(sorted(s))
+            sorted_t = ''.join(sorted(t))
+            if sorted_s == sorted_t:
+                return True
+            else:
+                return False
