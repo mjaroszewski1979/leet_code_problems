706023fc3526ab7d5b46284e287b6a16b34ff635
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-09-03 22:52:48 +0200

    added solution to move zeroes problem

--- move_zeroes.py
+++ move_zeroes.py
@@ -15,3 +15,15 @@ Input: nums = [0]
 Output: [0]
 
 '''
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for num in nums:
+            if num == 0:
+                idx = nums.index(num)
+                num_pop = nums.pop(idx)
+                nums.append(num_pop)
