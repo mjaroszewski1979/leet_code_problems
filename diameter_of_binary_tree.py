9c0dfb356990dc696e32cec38ee95de0a62dbdf0
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-10-03 20:14:08 +0200

    added solution to diameter of binary tree problem using dfs approach to save memory usage

--- diameter_of_binary_tree.py
+++ diameter_of_binary_tree.py
@@ -81,3 +81,25 @@ class Solution_3(object):
         height(root)
         
         return self.result
+
+class Solution_4(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        
+        self.ans = 0
+        self.dfs(root)
+        
+        return self.ans
+    
+    def dfs(self, node):
+        if not node:
+            return 0
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+        
+        self.ans = max(self.ans,right+left)
+        
+        return max(left+1,right+1)
