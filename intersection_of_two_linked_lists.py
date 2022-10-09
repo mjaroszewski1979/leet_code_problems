97e1f6f5407c18a9ab705f061f816d93a032f8ea
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-10-09 18:41:39 +0200

    added alternative solution to problem - intersection of two linked lists, using approach based on calculating difference between lengths of linked lists

--- intersection_of_two_linked_lists.py
+++ intersection_of_two_linked_lists.py
@@ -54,3 +54,41 @@ class Solution_2(object):
                 pointer_2 = headA
                 
         return pointer_1
+
+class Solution_3(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        def get_length(node):
+            count = 0
+            while node:
+                count += 1
+                node = node.next
+            return count
+        
+        length_a = get_length(headA)
+        length_b = get_length(headB)
+        
+        
+        if length_a > length_b:
+            diff = length_a - length_b
+            pointer_long = headA
+            pointer_short = headB
+            
+        else:
+            diff = length_b - length_a
+            pointer_long = headB
+            pointer_short = headA
+            
+        i = 0
+        while i < diff:
+            i += 1
+            pointer_long = pointer_long.next
+            
+        while pointer_long != pointer_short:
+            pointer_long = pointer_long.next
+            pointer_short = pointer_short.next
+            
+        return pointer_long
