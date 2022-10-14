aba94c7222b2626eeced1dd5d11262200698e954
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-10-14 20:53:19 +0200

    added alternative solutions to problem - longest palindromic substring

--- longest_palindromic_substring.py
+++ longest_palindromic_substring.py
@@ -58,3 +58,90 @@ class Solution_2(object):
             for start_index in range(0, len(s)+1-length):
                 if get_palindrome(s[start_index:(start_index+length)]):
                     return s[start_index:(start_index+length)]
+
+
+class Solution_3(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def get_palindrome(left,right):
+            while(left >= 0 and right < len(s) and s[left] == s[right]):
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        result = ''
+        for i in range(len(s)):
+            
+            temp = get_palindrome(i, i)
+            if len(temp) > len(result):
+                result = temp
+                
+            temp = get_palindrome(i, i+1)
+            if len(temp) > len(result):
+                result = temp
+                
+        return result
+
+class Solution_4(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def get_palindrome(s, left, right):
+            while(left >= 0 and right < len(s) and s[left] == s[right]):
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        result = ''
+        
+        for i in range(len(s)):
+            
+            odd = get_palindrome(s, i, i)
+            
+            even = get_palindrome(s, i, i+1)
+            
+            result = max(odd, even, result, key=len)
+            
+        return result
+
+class Solution_5(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def get_palindrome(s):
+            return (s == s[::-1])
+        
+        longest = s[0]
+        step = len(longest) // 2
+        
+        # single letter center
+        for center in range(1, len(s)-1):
+            bounds = [center-(1+step), center+(1+step)]
+            while(bounds[0] > -1 and bounds[1] < len(s)):
+                if get_palindrome(s[bounds[0]:bounds[1]+1]):
+                    longest = s[bounds[0]:bounds[1]+1]
+                    step = len(longest) // 2
+                    bounds[0] -= 1
+                    bounds[1] += 1
+                else:
+                    break
+        # double letter center      
+        for center in range(step, len(s)-step-1):
+            bounds = [center-(step), center+(1+step)]
+            while(bounds[0] > -1 and bounds[1] < len(s)):
+                if get_palindrome(s[bounds[0]:bounds[1]+1]):
+                    longest = s[bounds[0]:bounds[1]+1]
+                    step = len(longest) // 2
+                    bounds[0] -= 1
+                    bounds[1] += 1
+                else:
+                    break
+                    
+        return longest
