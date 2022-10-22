89c244ec2604f36bb8e59239d9edabc9c485c5ae
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-10-22 23:01:54 +0200

    added solution to problem - letter combinations of a phone number using list comprehension

--- letter_combinations_of_a_phone_number.py
+++ letter_combinations_of_a_phone_number.py
@@ -86,3 +86,33 @@ class Solution_2(object):
             result = temp
                 
         return result
+
+class Solution_3(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        
+        if not digits:
+            return []
+        
+        result = ['']
+        
+        data = {
+            '2' : 'abc',
+            '3' : 'def',
+            '4' : 'ghi',
+            '5' : 'jkl',
+            '6' : 'mno',
+            '7' : 'pqrs',
+            '8' : 'tuv',
+            '9' : 'wxyz'
+        }
+        
+        result = list(data[digits[0]])
+        
+        for digit in digits[1:]:
+            result = list(old + new for old in result for new in list(data[digit]))
+            
+        return result
