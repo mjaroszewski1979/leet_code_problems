c297eb0b2a2542e3dc5c73883404d1fa90230dba
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-11-01 18:20:04 +0100

    added alternative solution to same tree problem

--- same_tree.py
+++ same_tree.py
@@ -61,3 +61,19 @@ class Solution_2(object):
             return False
         
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+class Solution_3(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        if not p and not q:
+            return True
+        
+        if p and q:
+            return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
