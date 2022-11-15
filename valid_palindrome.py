dc8ecf0901dfa423f883014b217cf059802ab97f
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-11-15 21:09:49 +0100

    added solution to problem -valid palindrome- using two pointers and custom function for checking alpha numeric characters

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -38,3 +38,33 @@ class Solution_1(object):
                 result += char
         result = result.lower()
         return result == result[::-1]
+
+class Solution_2(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if s == '':
+            return True
+        
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            while left < right and not self.is_alpha(s[left]):
+                left += 1
+            while right > left and not self.is_alpha(s[right]):
+                right -= 1
+                
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+            
+        return True
+                
+        
+    def is_alpha(self, char):
+        return (ord('A') <= ord(char) <= ord('Z') or ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'))
