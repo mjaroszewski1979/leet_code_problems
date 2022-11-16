cc3b8c2ee612e4e9a26294fa62fbc9267acf9d77
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-11-16 20:24:30 +0100

    added recursive solution to problem -binary tree preorder traversal-

---
+++ binary_tree_preorder_traversal.py
@@ -0,0 +1,45 @@
+'''
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+
+'''
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def preorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        res = []
+        
+        def inorder(root):
+            if not root:
+                return
+            
+            res.append(root.val)
+            inorder(root.left)
+            inorder(root.right)
+            
+        inorder(root)
+        return res
